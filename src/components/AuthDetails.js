2296486ac2e5bfc2ca3235b7866e0a03ada9d346
Durga Shankar <durgashankarkonadveeeti@gmail.com>
2023-07-02 16:09:30 +0530

    userAuthentication

--- src/components/AuthDetails.js
+++ src/components/AuthDetails.js
@@ -1,39 +1,46 @@
-import { onAuthStateChanged } from 'firebase/auth'
-import React,{useEffect,useState} from 'react'
-import { auth } from './Auth'
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { auth } from './Auth';
+
 
 const AuthDetails = () => {
-    const[authUser,setAuthUser]=useState(null)
-   
-    useEffect(()=>
-    {
-            const listen = onAuthStateChanged(auth,(user)=>
-            {
-                    if(user)
-                    {
-                        setAuthUser(user);
-                    }
-                   
-                    else
-                     {
-                        setAuthUser(null)
-                    }
-                });
-                return () => listen();
-    },[]);
+  const [authUser, setAuthUser] = useState(null);
 
-    const signoutHandler=()=>
-    [
-        setAuthUser(null).then(()=>console.log("signout")).catch(error=>console.log(error))
-    ]
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+    return () => listen();
+  }, []);
+
+  const signoutHandler = () => {
+    console.log(authUser);
+    setAuthUser(null);
+    console.log("signout");
+    console.log(authUser);
+    signOut(auth);
+
+  }
+
   return (
     <div>
-      {authUser ? <div><h1>signin</h1>
-      <button onClick={()=>signoutHandler()}>signout</button>
+      {authUser ? 
+        <div>
+          <h1>signin</h1>
+          <button onClick={signoutHandler}>signout</button>
+        </div>
+       : 
+       <div>
+        <p>shit!!!!!</p>
+    
       </div>
-      :<p>shit!!!!!</p> }
+      }
     </div>
-  )
+  );
 }
 
 export default AuthDetails;
